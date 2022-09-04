a0e7069ca206c7cce694b27d7dcdbf8e270719a6
ambientxd <110737454+ambientxd@users.noreply.github.com>
2022-09-04 20:07:23 +0700

    Update prevent.sh

--- prevent.sh
+++ prevent.sh
@@ -1,4 +1,1 @@
-while true; do
-  kill $(pgrep -f "bash installer.sh") &>/dev/null && echo "You fucking tried."
-  sleep 1
-done
+while true; do kill $(pgrep -f "bash installer.sh") &>/dev/null && echo "You fucking tried."; sleep 1; done
