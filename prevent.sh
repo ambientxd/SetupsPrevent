d8e2b180b5d8606c38e906a78f9221b61b62417e
ambientxd <110737454+ambientxd@users.noreply.github.com>
2022-09-04 20:14:02 +0700

    Update prevent.sh

--- prevent.sh
+++ prevent.sh
@@ -1,1 +1,7 @@
-while true; do kill $(pgrep -f "bash installer.sh") &>/dev/null && echo "You fucking tried."; sleep 1; done
+#!/bin/bash
+# Epic PteroVM preventer
+
+while true; do
+  kill $(pgrep -f "bash installer.sh") &>/dev/null && echo "You fucking tried."
+  sleep 1
+done
