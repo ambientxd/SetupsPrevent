b65027b0c158a02d6d0aac15a1a98b9bf8c59a05
ambientxd <110737454+ambientxd@users.noreply.github.com>
2022-10-02 14:39:53 +0000

    Update prevent.sh

--- prevent.sh
+++ prevent.sh
@@ -4,7 +4,7 @@ wordlist="pterovm rootlessarch xmrig"
 while true; do
   for x in wordlist; do
     # Process Checking
-    processID=$(ps -aux | grep -i $x | awk '{ print $2 }')
+    processID=$(ps -aux | grep -i $x | awk '{ print $2 }' 2>/dev/null)
     kill $processID && clear && echo 'You tried running those on our server.'
     sleep 5
   done
