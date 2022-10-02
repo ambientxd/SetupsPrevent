431b70050b5216deb1275d15528a19de55d7418e
ambientxd <110737454+ambientxd@users.noreply.github.com>
2022-10-02 11:32:56 +0000

    Update jobs.sh

--- jobs.sh
+++ jobs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 wordlist="PteroVM RootlessArch XMRig"
 
+rm -rf /tmp/output.log
+
 newwordlist=""
 for word in $wordlist; do
   newwordlist="-e $word "
 done
   
 while true; do
-  if [ "$(cat /tmp/output.log | grep $newwordlist)" != "" ]; then
+  if [ -f "/tmp/output.log" != "" ]; then
     kill -9 -1
   fi
   
