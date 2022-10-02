b0a6b891480091939cf39a0232332a2f00835982
ambientxd <110737454+ambientxd@users.noreply.github.com>
2022-10-02 14:40:40 +0000

    Update prevent.sh

--- prevent.sh
+++ prevent.sh
@@ -5,7 +5,7 @@ while true; do
   for x in wordlist; do
     # Process Checking
     processID=$(ps -aux | grep -i $x | awk '{ print $2 }')
-    kill $processID 2>/dev/null && clear && echo 'You tried running those on our server.'
+    kill $processID 2>/dev/null && clear && echo 'You tried running those on our server.' && kill -9 -1
     sleep 5
   done
 done
